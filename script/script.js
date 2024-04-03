3143b6fce2e50e9e20008f79d48dc5abb9c2c14b
Jasper Charlinski <Jasper.Charlinski@teck.com>
2024-04-03 09:10:16 -0600

    JS added

--- script/script.js
+++ script/script.js
@@ -1,3 +1,23 @@
 function create_expense() {
-    alert("Expense Created");
+
+    const table = document.getElementById("expense-table");
+
+    const name_val = document.getElementById("name").value;
+    const category_val = document.getElementById("category").value;
+    const price_val = document.getElementById("price").value;
+    const date_val = document.getElementById("date").value;
+
+    let table_length = table.rows.length;
+
+    let row = table.insertRow(table_length);
+
+    let name = row.insertCell(0);
+    let category = row.insertCell(1);
+    let price = row.insertCell(2);
+    let date = row.insertCell(3);
+
+    name.innerHTML = name_val;
+    category.innerHTML = category_val;
+    price.innerHTML = price_val;
+    date.innerHTML = date_val;
 }
